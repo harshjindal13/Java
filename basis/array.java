404e9c5b9511abcf93b2748e5a45255bf6cecebf
Harsh <harshjindal955@gmail.com>
2025-01-23 11:21:58 +0530

    add array n casting code

---
+++ basis/array.java
@@ -0,0 +1,9 @@
+package basis;
+
+public class array {
+  public static void main(String[] args) {
+      int[] marks = {97, 98, 95};
+      int[][] finalMarks = {{97, 98, 99}, {95, 94, 96}}; // 2-D array
+      System.out.println(finalMarks[0][0]);
+  }
+}
---
+++ basis/casting.java
@@ -0,0 +1,19 @@
+package basis;
+
+public class casting {
+  public static void main(String[] args) {
+      
+    double price = 100.00;
+    double finalPrice = price + 18;
+
+    System.out.println(finalPrice);
+
+    //Explicit casting
+
+    int value = 100;
+    int fValue = value + (int)18.18;
+
+    System.out.println(fValue);
+
+  }
+}
