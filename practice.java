cd18ce542ece03e877092d6f04fa311a13fcd4ac
Harsh <harshjindal955@gmail.com>
2025-01-23 15:07:51 +0530

    add guess the ouptput game

--- practice.java
+++ practice.java
@@ -1,9 +1,25 @@
-// Making a guess the output game in which user have to guess the randomly generated number, and type -1 to stop the game.
-
-
 import java.util.Scanner;
 
-
 public class practice {
-  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int myNumber = (int)(Math.random() * 100);
+        int userNumber = 0;
+
+        do {
+            System.out.println("Guess the number: ");
+            userNumber = sc.nextInt();
+
+            if (userNumber == myNumber) {
+                System.out.println("WooHoo...Correct Number!");
+                break;
+            } else if (userNumber > myNumber) {
+                System.out.println("Your number is too large!");
+            } else {
+                System.out.println("Your number is too small!");
+            }
+        } while (userNumber >= 0);
+
+        System.out.println("My number was: " + myNumber);
+    }
 }
