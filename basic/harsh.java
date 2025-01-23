2a916b34561d31c2793aafaed68a048412536d43
Harsh <harshjindal955@gmail.com>
2025-01-23 10:41:52 +0530

    add basis file

---
+++ basic/harsh.java
@@ -0,0 +1,14 @@
+
+public class harsh {
+  public static void main(String[] args) {
+    System.out.println("harsh was here \n");
+    System.out.println("hello");
+
+    String name = "harsh";
+    String surname = "jindal";
+    String name1 = name.replace("harsh", "KANCHA");
+
+    System.out.println(name1.substring(1, 4));
+    System.out.println(name + surname);
+  }
+}
