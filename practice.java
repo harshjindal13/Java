696ce3675f3d82d41328aeefd246cc18b351cadd
Harsh <harshjindal955@gmail.com>
2025-01-23 14:50:21 +0530

    initialize guess the output project

---
+++ practice.java
@@ -0,0 +1,9 @@
+// Making a guess the output game in which user have to guess the randomly generated number, and type -1 to stop the game.
+
+
+import java.util.Scanner;
+
+
+public class practice {
+  
+}
